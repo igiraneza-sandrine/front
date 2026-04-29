87b88e86c882c3633de3e3a8d6439940a0c55f8f
sandra <igiranezaimesand@gmail.com>
2026-04-29 06:07:02 +0200

    first commit

---
+++ pages/integration.js
@@ -0,0 +1,6 @@
+// import integrate from './src/api/axios.js';
+// async function handleLogin(email,password){
+//     try{
+//         const response=await integrate.post('/auth/log')
+//     }
+// }
